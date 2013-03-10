fdb8e607c46fce2cc6bc107dbce5dde90a24c084
William Cheung <wcheung@irise.com>
2013-03-09 20:35:22 -0500

    Don't escape quotes in JSON.

--- app.js
+++ app.js
@@ -342,8 +342,9 @@ function processRequest(req, res, next) {
 
     if (['POST','DELETE','PUT'].indexOf(httpMethod) !== -1) {
         var requestBody = JSON.stringify(params);
-        requestBody = requestBody.replace('"[', '[');
-        requestBody = requestBody.replace(']"', ']');
+        requestBody = requestBody.replace(/"\[/g, '['); // "[ -> [
+        requestBody = requestBody.replace(/\]"/g, ']'); // ]" -> ]
+        requestBody = requestBody.replace(/\\"/g, '"'); // \" -> "
         console.log('requestBody: ' + requestBody);
     }
 
